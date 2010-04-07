c7035218f2b9d3a465f045a6fe535f2c3596ccfa
eirikluka <thaerk@gmail.com>
2010-04-07 12:18:24 +0200

    Fikset i database. Lagt til spillere og ordnet lag

--- database.h
+++ database.h
@@ -16,26 +16,30 @@ static bool createConnection(){
     }
 
     QSqlQuery query;
+
+    query.exec("DROP TABLE lag");
+    query.exec("DROP TABLE spiller");
+
     query.exec("CREATE TABLE lag (id INT PRIMARY KEY,"
                                  "lagnavn VARCHAR(40),"
                                  "lagInfo VARCHAR(50))");
 
-    query.exec("INSERT INTO lag VALUES(0, 'Vålerenga', 'Etablert i 1998')");
-    query.exec("INSERT INTO lag VALUES(1, 'Rosenborg', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(2, 'Odd Grenland', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(3, 'Molde', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(4, 'Stabæk', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(5, 'Tromsø', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(6, 'Aalesund', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(7, 'Start', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(8, 'Lillestrøm', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(9, 'Haugesund', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(10, 'Viking', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(11, 'Sandefjord', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(12, 'Kongsvinger', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(13, 'Hønefoss', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(14, 'Brann', 'Etablert i 1992')");
-    query.exec("INSERT INTO lag VALUES(15, 'Strømsgodset', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(0, 'Aalesund', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(1, 'Brann', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(2, 'Haugesund', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(3, 'Hønefoss', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(4, 'Kongsvinger', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(5, 'Lillestrøm', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(6, 'Molde', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(7, 'Odd Grenland', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(8, 'Rosenborg', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(9, 'Sandefjord', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(10, 'Stabæk', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(11, 'Start', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(12, 'Strømsgodset', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(13, 'Tromsø', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(14, 'Viking', 'Etablert i 1992')");
+    query.exec("INSERT INTO lag VALUES(15, 'Vålerenga', 'Etablert i 1998')");
 
     query.exec("CREATE TABLE spiller (id INT PRIMARY KEY,"
                                      "fornavn VARCHAR(40),"
@@ -45,8 +49,11 @@ static bool createConnection(){
                                      "lagID INT,"
                                      "FOREIGN KEY (lagID) REFERENCES lag(id))");
 
-    query.exec("INSERT INTO spiller VALUES(0, 'Jon Torstein', 'Dalen', '3', 'Høyre back', 2)");
-    query.exec("INSERT INTO spiller VALUES(1, 'Eirik', 'Luka', '7', 'Spiss', 2)");
+    query.exec("INSERT INTO spiller VALUES(null, 'Jon Torstein', 'Dalen', '3', 'Høyre back', 7)");
+    query.exec("INSERT INTO spiller VALUES(null, 'Eirik', 'Luka', '7', 'Spiss', 15)");
+    query.exec("INSERT INTO spiller VALUES(null, 'Anders', 'Evenstuen', '7', 'Spiss', 8)");
+    query.exec("INSERT INTO spiller VALUES(null, 'Henrik', 'Wingerei', '4', 'Midtbane', 10)");
+    query.exec("INSERT INTO spiller VALUES(null, 'Øyvind', 'Midtbø', '1', 'Keeper', 4)");
 
     return true;
 }
